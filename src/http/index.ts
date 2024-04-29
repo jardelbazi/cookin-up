570d0ce8c11ccfce9c9a289d2a4ef79b12be4475
Jardel Pavan Bazi <jardelbazi@gmail.com>
2024-04-29 11:22:56 -0300

    feat: Mostrar e Buscar Receitas

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,15 @@
 import type ICategoria from "@/interfaces/ICategoria"
+import type IReceita from "@/interfaces/IReceita"
+
+async function obterDadosURL<T>(url: string) {
+  const resposta = await fetch(url)
+  return resposta.json() as T
+}
 
 export async function obterCategorias() {
-  const resposta = await fetch('https://gist.githubusercontent.com/jardelbazi/794a5a03e7b2fb6fe6fccf0ee78d6a9c/raw/7954177f41c4100f57f66be48549a0fba3e0ea29/categorias.json')
-  const categorias: ICategoria[] = await resposta.json()
+  return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/jardelbazi/794a5a03e7b2fb6fe6fccf0ee78d6a9c/raw/7954177f41c4100f57f66be48549a0fba3e0ea29/categorias.json')
+}
 
-  return categorias
+export async function obterReceitas() {
+  return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/jardelbazi/edacba4b89c98d1ade8458c09516b5dc/raw/8264273ff8acfd07226ae460b24c0ee0cc140ba1/receitas.json')
 }
